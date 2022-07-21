6bf9571189cc580cac7e7dd932dcf65316a114e4
Maksym_Odnokozov <Maksym_Odnokozov@epam.com>
2022-07-21 09:55:21 +0300

    task1: added training materials

---
+++ task_1/src/main/java/training/ReplaceValue.java
@@ -0,0 +1,23 @@
+package training;
+
+/**
+ * We have three variables x,y,z. x & z variable initialized with value 10 and 15.
+ * You need to replace the values for these variables using y variable like buffer.
+ * After that printed result condition should be true.
+ * You should not use implicit set value, like z = 10 and x = 15 or something like that.
+ * Any interactions with numbers(digits)
+ */
+public class ReplaceValue {
+
+    public static void main(String[] args) {
+        //given
+        int x = 10;
+        int z = 15;
+        int y;
+
+        //Todo put there you code;
+
+
+        System.out.println(x == 15 && z == 10);
+    }
+}
---
+++ task_1/src/main/java/training/ReversedArray.java
@@ -0,0 +1,38 @@
+package training;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/*
+ * Needs to implement method getReversedArray using just only for construction.
+ */
+public class ReversedArray {
+
+    private static final int DEFAULT_ARRAY_SIZE = 10;
+    private static final int MAX_INT_VALUE = 1000;
+
+    public static void main(String[] args) {
+        int[] arr = getArrayWithRandomDigits();
+        System.out.println("given array");
+        System.out.println(Arrays.toString(arr));
+
+        arr = getReversedArray(arr);
+
+        System.out.println("reversed array");
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static int[] getArrayWithRandomDigits() {
+        int[] arr = new int[DEFAULT_ARRAY_SIZE];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new Random().nextInt(MAX_INT_VALUE);
+        }
+        return arr;
+    }
+
+    //Todo add your implementation
+    private static int[] getReversedArray(int[] arr) {
+        return null;
+    }
+}
