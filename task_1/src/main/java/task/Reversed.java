9f3651a0f459a933dcb39ca6b3bb7f85fdd6dca9
wereva <wereva97@gmail.com>
2022-08-14 12:02:43 +0300

    Code refactor

--- task_1/src/main/java/task/Reversed.java
+++ task_1/src/main/java/task/Reversed.java
@@ -1,6 +1,9 @@
+package task;
+
 import java.util.Scanner;
 
 public class Reversed {
+
         public static void main(String[] args) {
             System.out.println("Enter to string\n");
             Scanner scr = new Scanner(System.in);
@@ -15,22 +18,22 @@ public class Reversed {
             System.out.println("String after revers\n\n" + s);
         }
 
-        public static String getReversedString(String input) {
-            char[] chars = input.toCharArray();
-            String wordReversed = String.valueOf(getReversedLettersArr(chars));
-            StringBuilder wordTwoResult = new StringBuilder();
-            char[] wordReversedArray = wordReversed.toCharArray();
+    public static String getReversedString(String input) {
+        char[] chars = input.toCharArray();
+        String wordReversed = String.valueOf(getReversedLettersArr(chars));
+        StringBuilder wordResult = new StringBuilder();
+        char[] wordReversedArray = wordReversed.toCharArray();
 
-            for (int i = 0, j = 0; i < chars.length; i++) {
-                if (!Character.isLetter(chars[i])) {
-                    wordTwoResult.append(chars[i]);
-                    j++;
-                } else {
-                    wordTwoResult.append(wordReversedArray[i - j]);
-                }
+        for (int i = 0, j = 0; i < chars.length; i++) {
+            if (!Character.isLetter(chars[i])) {
+                wordResult.append(chars[i]);
+                j++;
+            } else {
+                wordResult.append(wordReversedArray[i - j]);
             }
-            return wordTwoResult.toString();
         }
+        return wordResult.toString();
+    }
 
         public static char[] getReversedLettersArr(char[] chars) {
             StringBuilder word = new StringBuilder();
@@ -48,7 +51,7 @@ public class Reversed {
             StringBuilder result = new StringBuilder();
             for (String s : input) {
                 result.append(s);
-                result.append("  ");
+                result.append(" ");
             }
             return result.toString();
         }
