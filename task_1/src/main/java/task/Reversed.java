b49a1ce62c0ccfe293d4e910ad3ecd0c31c088c0
Slavik <93665700+wereva@users.noreply.github.com>
2022-08-19 20:07:15 +0300

    Task1:Resolved comments

--- task_1/src/main/java/task/Reversed.java
+++ task_1/src/main/java/task/Reversed.java
@@ -4,20 +4,19 @@ import java.util.Scanner;
 
 public class Reversed {
 
-        public static void main(String[] args) {
-            System.out.println("Enter to string\n");
-            Scanner scr = new Scanner(System.in);
-            String input = scr.nextLine();
-            String[] word = splitStringIntoWords(input);
-            System.out.println("\nString after revers\n\n" + buildResult(word));
-        }
+    public static void main(String[] args) {
+        System.out.println("Enter to string\n");
+        Scanner scr = new Scanner(System.in);
+        String input = scr.nextLine();
+        System.out.println("\nString after revers\n\n" + getReversedString(input));
+    }
 
-    public static String[] splitStringIntoWords(String input) {
-        String[] word = input.split(" ");
-        for (int i = 0; i < word.length; i++) {
-            word[i] = getReversedWord(word[i]);
+    public static String getReversedString(String input) {
+        String[] words = input.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            words[i] = getReversedWord(words[i]);
         }
-        return word;
+        return buildResult(words);
     }
 
     public static String getReversedWord(String word) {
@@ -37,26 +36,25 @@ public class Reversed {
         return wordResult.toString();
     }
 
-        public static char[] getReversedLettersArr(char[] chars) {
-            StringBuilder word = new StringBuilder();
-
-            for (char currentChar : chars) {
-                if (Character.isLetter(currentChar)) {
-                    word.append(currentChar);
-                }
+    public static char[] getReversedLettersArr(char[] chars) {
+        StringBuilder word = new StringBuilder();
+        for (char currentChar : chars) {
+            if (Character.isLetter(currentChar)) {
+                word.append(currentChar);
             }
-            return word.reverse().toString().toCharArray();
         }
-
-        public static String buildResult(String[] input) {
-            StringBuilder result = new StringBuilder();
-            for (String s : input) {
-                result.append(s);
-                result.append(" ");
-
-            }
-            return result.toString().trim();
-        }
+        return word.reverse().toString().toCharArray();
     }
+
+    public static String buildResult(String[] input) {
+        StringBuilder result = new StringBuilder();
+        for (String s : input) {
+            result.append(s);
+            result.append(" ");
+
+        }
+        return result.toString().trim();
+    }
+}
 
 
